eab5cc18290bd90a4d6170319a70953ee69c6e57
Abhishek <abhishekkambli3@gmail.com>
2020-11-16 21:34:21 +0530

    will it work?

--- src/pages/features.jsx
+++ src/pages/features.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import Image from "../components/Image"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {
